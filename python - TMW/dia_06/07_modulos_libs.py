37cd8d2b33554ce011e15fb7563497df81ed203a
Passarete <lucas-passarete@hotmail.com>
2026-01-21 11:09:53 -0300

    feat:modulos e libs

---
+++ python - TMW/dia_06/07_modulos_libs.py
@@ -0,0 +1,19 @@
+#%%
+import math
+from math import pi, e
+import math as mh
+# %%
+math.sqrt(9)
+# %%
+math.pow(2, 4)
+# %%
+math.e
+# %%
+math.pi
+# %%
+pi
+# %%
+e
+# %%
+print(mh.factorial(4))
+# %%
---
+++ python - TMW/dia_06/08_aleatorio.py
@@ -0,0 +1,10 @@
+#%%
+import random
+
+# %%
+random.randint(1, 10)
+
+#%%
+X= ['pessoa1', 'pessoa2', 'pessoa3', 'pessoa4']
+random.choice(X)
+# %%
