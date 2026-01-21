36a6372de08c4120691c0ff9e9eec2d252026014
Passarete <lucas-passarete@hotmail.com>
2026-01-21 11:28:49 -0300

    feat: dia_7

---
+++ python - TMW/dia_07/read.py
@@ -0,0 +1,17 @@
+#%%
+nome_arquivo = "C:/Users/lucas/AQUI PRA CLONAR/Python-EX/python - TMW/dia_07/historia.txt"
+
+# abre arquivo em formato de leitura
+open_file = open(nome_arquivo)
+# %%
+# le os dados do arquivo
+conteudo = open_file.read()
+print(conteudo)
+# %%
+# fecha o arquivo
+open_file.close()
+# %%
+# maneira correta de abrir e fechar leitura de arquivos.
+with open(nome_arquivo) as open_file:
+    conteudo = open_file.read()
+print(conteudo)
